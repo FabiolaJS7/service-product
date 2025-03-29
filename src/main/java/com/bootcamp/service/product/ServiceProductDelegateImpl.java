48dc09f70cac1aa6a8f87d00d49cd9dd72c24799
Monica Fabiola Garcia Saravia <mgarcsar@emeal.nttdata.com>
2025-03-29 00:02:27 -0500

    first commit

---
+++ src/main/java/com/bootcamp/service/product/ServiceProductDelegateImpl.java
@@ -0,0 +1,14 @@
+package com.bootcamp.service.product;
+
+
+import com.bootcamp.service.product.api.ApiApiDelegate;
+import com.bootcamp.service.product.model.ProductRequest;
+import com.bootcamp.service.product.model.ProductResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceProductDelegateImpl implements ApiApiDelegate {
+
+
+}
